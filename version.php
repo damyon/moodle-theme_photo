56f562efae125ab3640ce3a060aa532878c193b5
Damyon Wiese <damyon@moodle.com>
2016-11-30 14:55:55 +0800

    Bump dependency versions to latest.

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = '2016102100';
+$plugin->version = '2016113000.00';
 
 // This is the version of Moodle this plugin requires.
-$plugin->requires = '2016070700';
+$plugin->requires = '2016112900.00';
 
 // This is the component name of the plugin - it always starts with 'theme_'
 // for themes and should be the same as the name of the folder.
